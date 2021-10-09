be5f8fabde7fd8a1c4ed85358e468a5e2739a00f
Matej Pesjak <matej.pesjak@equaleyes.com>
2021-10-09 09:22:48 +0200

    chor:Removed functions for bits to class

---
+++ RacunalniskaMultimedija/vaja1-okt8/bit.py
@@ -0,0 +1,24 @@
+class Bit:
+    def __init__(self):
+        self.bits = []
+
+    # Read bits from byte value
+    def read_bits(self, byte_value):
+        bit_array = []
+
+        # TODO: Add byte to bit magic
+
+        self.bits.append(bit_array)
+
+    # Write bits from bit array
+    def write_bits(self, filepath):
+        return
+
+    def find_bit_positions(self, bit_array, bit_to_find):
+        return
+
+    def find_and_replace_bit(self, old_bit_value, new_bit_value):
+        return
+
+    def get_bits(self):
+        return self.bits
--- RacunalniskaMultimedija/vaja1-okt8/readBit.py
+++ RacunalniskaMultimedija/vaja1-okt8/readBit.py
@@ -1,44 +1,31 @@
 import sys
+from bit import Bit
 
-# Read bits from byte value
-def read_bits(byte_value):
-    bit_array = []
-    return bit_array
+if __name__ == "__main__":
+    my_bit = Bit()
 
+    # Get filename from arguments
+    filename = sys.argv[1]
+    option = sys.argv[2]
 
-# Write bits from bit array
-def write_bits(bit_array, filepath):
-    return
+    # Check for option and correcsponding argument length
+    if option not in ["f", "fr"]:
+        print("Wrong option parameter")
+        exit()
+    elif option == "f" and len(sys.argv) != 4:
+        print("Wrong number of parameters")
+        exit()
+    elif option == "fr" and len(sys.argv) != 5:
+        print("Wrong number of parameters")
+        exit()
 
-
-def find_bit_positions(bit_array, bit_to_find):
-    return
-
-
-def find_and_replace_bit(old_bit_value, new_bit_value):
-    return
-
-
-# Get filename from arguments
-filename = sys.argv[1]
-option = sys.argv[2]
-
-# Check for option and correcsponding argument length
-if option not in ["f", "fr"]:
-    print("Wrong option parameter")
-    exit()
-elif option == "f" and len(sys.argv) != 4:
-    print("Wrong number of parameters")
-    exit()
-elif option == "fr" and len(sys.argv) != 5:
-    print("Wrong number of parameters")
-    exit()
-
-# Open filename
-with open(filename, "rb") as read_file:
-    # First byte
-    byte_value = read_file.read(1)
-    # Write whole file byte by byte
-    while byte_value != b"":
+    # Open filename
+    with open(filename, "rb") as read_file:
+        # First byte
         byte_value = read_file.read(1)
-        print(byte_value)
+        my_bit.read_bits(byte_value=byte_value)
+        # Write whole file byte by byte
+        while byte_value != b"":
+            byte_value = read_file.read(1)
+            my_bit.read_bits(byte_value=byte_value)
+    print(my_bit.get_bits())

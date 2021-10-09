1f4ed7fc8a297b5327db1cc5aec32309f05e3a3a
Matej Pesjak <matej.pesjak@equaleyes.com>
2021-10-09 09:13:26 +0200

    chor: Added function skeleton for Multimedia vaje1

--- RacunalniskaMultimedija/vaja1-okt8/readBit.py
+++ RacunalniskaMultimedija/vaja1-okt8/readBit.py
@@ -1,5 +1,25 @@
 import sys
 
+# Read bits from byte value
+def read_bits(byte_value):
+    bit_array = []
+    return bit_array
+
+
+# Write bits from bit array
+def write_bits(bit_array, filepath):
+    return
+
+
+def find_bit_positions(bit_array, bit_to_find):
+    return
+
+
+def find_and_replace_bit(old_bit_value, new_bit_value):
+    return
+
+
+# Get filename from arguments
 filename = sys.argv[1]
 option = sys.argv[2]
 
@@ -14,9 +34,11 @@ elif option == "fr" and len(sys.argv) != 5:
     print("Wrong number of parameters")
     exit()
 
+# Open filename
 with open(filename, "rb") as read_file:
+    # First byte
     byte_value = read_file.read(1)
-    print(byte_value)
+    # Write whole file byte by byte
     while byte_value != b"":
         byte_value = read_file.read(1)
         print(byte_value)

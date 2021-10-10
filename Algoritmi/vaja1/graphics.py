0da59b7a32a877e18e5942b660f5cc16602f2bc0
Matej Pesjak <matej.pesjak@equaleyes.com>
2021-10-10 13:00:54 +0200

    chor: Added finished GUI

--- Algoritmi/vaja1/graphics.py
+++ Algoritmi/vaja1/graphics.py
@@ -6,6 +6,7 @@ import miller_rabin
 screen = Tk()
 prime_number_input = IntVar()
 optional_parameter_s_input = IntVar()
+prime_number_length_input = IntVar()
 
 
 def check_prime_number_with_miller():
@@ -30,6 +31,24 @@ def check_prime_number_function_with_naive():
     result.place(x=15, y=220)
 
 
+def generate_prime_number():
+    digit_length = prime_number_length_input.get()
+    if digit_length < 2 or digit_length > 32:
+        result = Label(
+            text=f"Please insert a valid prime number in bit length from 2 fo 32",
+            bg="red",
+            width="50",
+        )
+    else:
+        prime_number = naive_function.naive_method(2 ** (digit_length - 1))
+        result = Label(
+            text=f"Your {digit_length} digit bit prime number is: {prime_number}",
+            bg="green",
+            width="50",
+        )
+    result.place(x=15, y=510)
+
+
 def create_graphics():
     screen.geometry("600x600")
     screen.title("Vaja 1")
@@ -38,13 +57,15 @@ def create_graphics():
     )
     heading.pack()
 
-    check_prime_number = Label(text="Check if number is primer number")
+    # Label for - Check prime number
+    check_prime_number = Label(text="Check if number is prime number")
     check_prime_number.place(x=15, y=70)
 
     prime_number_checker_entry = Entry(textvariable=prime_number_input, width="30")
 
     prime_number_checker_entry.place(x=15, y=90)
 
+    # Label for - Optional millers parameter s
     optional_parameter_s = Label(text="Optional parameter s for millers method")
     optional_parameter_s.place(x=15, y=110)
 
@@ -54,7 +75,8 @@ def create_graphics():
 
     optional_parameter_s_entry.place(x=15, y=130)
 
-    check_primer_number_miller_rabin = Button(
+    # Button for - Miller rabin method
+    check_prime_number_miller_rabin = Button(
         screen,
         text="Check with Miller Rabin's method",
         width="27",
@@ -62,7 +84,10 @@ def create_graphics():
         command=check_prime_number_with_miller,
         bg="grey",
     )
-    check_primer_number_naive = Button(
+    check_prime_number_miller_rabin.place(x=15, y=155)
+
+    # Label for - naive method
+    check_prime_number_naive = Button(
         screen,
         text="Check with Naive method",
         width="27",
@@ -70,7 +95,26 @@ def create_graphics():
         command=check_prime_number_function_with_naive,
         bg="grey",
     )
-    check_primer_number_miller_rabin.place(x=15, y=155)
-    check_primer_number_naive.place(x=15, y=185)
+    check_prime_number_naive.place(x=15, y=185)
+
+    # Label for - generating prime number
+    prime_number_length = Label(
+        text="How many digits do you want in your primary number (specify bit length 2 - 32)"
+    )
+    prime_number_length.place(x=15, y=400)
+
+    prime_number_length_entry = Entry(
+        textvariable=prime_number_length_input, width="30"
+    )
+    prime_number_length_entry.place(x=15, y=430)
+    generate_prime_number_button = Button(
+        screen,
+        text="Generate prime number",
+        width="27",
+        height="1",
+        command=generate_prime_number,
+        bg="grey",
+    )
+    generate_prime_number_button.place(x=15, y=455)
 
     screen.mainloop()

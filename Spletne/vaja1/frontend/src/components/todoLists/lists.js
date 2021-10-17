c94441e9d1ee9083f3243b608fd8635d874673dd
Matej Pesjak <matej.pesjak@equaleyes.com>
2021-10-18 00:04:29 +0200

    chor: Added DONE switch

--- Spletne/vaja1/frontend/src/components/todoLists/lists.js
+++ Spletne/vaja1/frontend/src/components/todoLists/lists.js
@@ -5,6 +5,7 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { FormControlLabel, Switch } from "@mui/material";
 
 export const Lists = ({ setListItems, listItems }) => {
   const [expanded, setExpanded] = React.useState(false);
@@ -30,17 +31,64 @@ export const Lists = ({ setListItems, listItems }) => {
     }).then((res) => {
       if (res.status === 204) {
         setListItems(listItems.filter((listItem) => listItem.id !== id));
-        // TODO Remove from array
       }
     });
+  };
+
+  const updateChor = async (id, name, until, done) => {
+    const res = await fetch("http://localhost:8080/chor", {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id, name, until, done }),
+    });
+
+    if (res.status === 200) {
+      return true;
+    }
+    return false;
+  };
+
+  const updateListItems = async (chorId, value) => {
+    const updated = await updateChor(chorId, null, null, value);
+
+    if (updated) {
+      const updatedListItems = listItems.map((list) => {
+        const updatedChors = list.chor.map((listChor) => {
+          if (listChor.id === chorId) {
+            listChor.done = value;
+          }
+          return listChor;
+        });
+
+        list.chor = updatedChors;
+        return list;
+      });
+      setListItems(updatedListItems);
+    }
   };
 
   const renderLists = () => {
     const lists = listItems.map((listItem, index) => {
       const chors = listItem.chor.map((chor, index) => {
         return (
-          <AccordionDetails key={index}>
-            <Typography>{chor.name}</Typography>
+          <AccordionDetails key={index} sx={{ display: "flex" }}>
+            <Typography sx={{ width: "33%", flexShrink: 0 }}>
+              {chor.name}
+            </Typography>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={chor.done}
+                  onClick={() => {
+                    updateListItems(chor.id, !chor.done);
+                  }}
+                />
+              }
+              label="Done"
+            />
           </AccordionDetails>
         );
       });

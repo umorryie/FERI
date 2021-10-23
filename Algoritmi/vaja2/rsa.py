56bdfa8dd1f1895d0f8dde3138e179c07cb7e711
Matej Pesjak <matej.pesjak@equaleyes.com>
2021-10-23 11:01:57 +0200

    chor : Added encrypt decrypt

--- Algoritmi/vaja2/rsa.py
+++ Algoritmi/vaja2/rsa.py
@@ -1,8 +1,14 @@
 import miller_rabin
 import extended_euclid
+import sys
+import math
+
 
 PUBLIC_KEY = "pubkey.txt"
 PRIVATE_KEY = "privkey.txt"
+ENCRYPTION_FILE = "encrypted.txt"
+DENCRYPTION_FILE = "decrypted.txt"
+ENCRYPTION_DECRYPTION_BIT_LENGTH = "length.txt"
 
 
 def get_two_prime_numbers(bit_length: int):
@@ -19,6 +25,7 @@ def get_e(fi_n: int) -> int:
             return i
 
 
+# TODO This doest work
 def modular_linear_equation_solver(a: int, b: int, n: int) -> int:
     extended_e = extended_euclid.ExtendedEuclid()
     d = extended_e.extended_euclid(a, n)
@@ -29,13 +36,96 @@ def modular_linear_equation_solver(a: int, b: int, n: int) -> int:
         print("Modular linear equation is not solvable")
 
 
+def from_string_to_bits(string_value: str) -> list:
+    bits_array = []
+    for character in string_value:
+        bits = bin(ord(character))[2:]
+        bits = "00000000"[len(bits) :] + bits
+        bits_array.extend([int(single_bit) for single_bit in bits])
+    return bits_array
+
+
+def from_bits_to_string(bits_value: list) -> str:
+    chars_array = []
+    for b in range(int(len(bits_value) / 8)):
+        byte = bits_value[b * 8 : (b + 1) * 8]
+        chars_array.append(chr(int("".join([str(bit) for bit in byte]), 2)))
+    return "".join(chars_array)
+
+
+def from_bit_to_int(bits_array: list) -> int:
+    return int("".join(str(x) for x in bits_array), 2)
+
+
+def from_int_to_bit(bit_int: int) -> list:
+    return [int(digit_number) for digit_number in bin(bit_int)[2:]]
+
+
+def from_array_to_chunk_array(iterable_array, chunk_size):
+    results = []
+    chunk = []
+    for i, el in enumerate(iterable_array, 1):
+        chunk.append(el)
+        if i % chunk_size == 0:
+            results.append(chunk)
+            chunk = []
+    if len(chunk) != 0:
+        results.append(chunk)
+
+    return results
+
+
 if __name__ == "__main__":
-    p, q = get_two_prime_numbers(bit_length=32)
-    n = q * p
-    fi_n = (q - 1) * (p - 1)
-    e = get_e(fi_n)
-    d = modular_linear_equation_solver(e, 1, fi_n)
-    with open(PUBLIC_KEY, "w") as f:
-        f.write(f"{e} {n}\n")
-    with open(PRIVATE_KEY, "w") as f:
-        f.write(f"{d} {n}\n")
+    if len(sys.argv) == 3 and sys.argv[1] == "encrypt" and ".txt" in sys.argv[2]:
+        p, q = get_two_prime_numbers(bit_length=32)
+        n = q * p
+        fi_n = (q - 1) * (p - 1)
+        e = get_e(fi_n)
+        d = modular_linear_equation_solver(e, 1, fi_n)
+        with open(PUBLIC_KEY, "w") as f:
+            f.write(f"{e} {n}\n")
+        with open(PRIVATE_KEY, "w") as f:
+            f.write(f"{d} {n}\n")
+        print("Public and private key successfully generated")
+
+        encode_length = int(math.log(n, 2)) + 1
+        with open(ENCRYPTION_DECRYPTION_BIT_LENGTH, "w") as f:
+            f.write(str(encode_length))
+
+        filename = sys.argv[2]
+        with open(filename, "r") as f:
+            lines = f.readlines()
+        bits_arrays = from_string_to_bits(lines[0])
+
+        for bit_chunk in from_array_to_chunk_array(bits_arrays, encode_length):
+            M = from_bit_to_int(bit_chunk)
+            C = miller_rabin.modular_exponentiation(M, e, n)
+            encrypted_value = from_bits_to_string(from_int_to_bit(C))
+            encrypted_file = open(ENCRYPTION_FILE, "a")
+            encrypted_file.write(encrypted_value)
+            encrypted_file.close()
+
+    elif len(sys.argv) == 2 and sys.argv[1] == "decrypt":
+        with open(DENCRYPTION_FILE, "w") as fp:
+            pass
+
+        with open(ENCRYPTION_DECRYPTION_BIT_LENGTH, "r") as f:
+            encode_length = int(f.readlines()[0])
+
+        with open(ENCRYPTION_FILE, "r") as f:
+            lines = f.readlines()
+        bits_arrays = from_string_to_bits(lines[0])
+
+        with open(PRIVATE_KEY, "r") as f:
+            line = f.readline()
+            line = line.split(" ")
+            d = int(line[0])
+            n = int(line[1])
+
+        for bit_chunk in from_array_to_chunk_array(bits_arrays, encode_length):
+            C = from_bit_to_int(bit_chunk)
+            M = miller_rabin.modular_exponentiation(C, d, n)
+            decrypted_value = from_bits_to_string(from_int_to_bit(M))
+            decrypted_file = open(DENCRYPTION_FILE, "a")
+            decrypted_file.write(decrypted_value)
+            decrypted_file.close()

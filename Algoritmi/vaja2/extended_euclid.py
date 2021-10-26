0adf9efa018ee2a487e67a4d06cd96b4e413c7ed
Matej Pesjak <matej.pesjak@equaleyes.com>
2021-10-26 23:18:18 +0200

    chor: Added right calculation of private and public key

--- Algoritmi/vaja2/extended_euclid.py
+++ Algoritmi/vaja2/extended_euclid.py
@@ -3,22 +3,17 @@ class ExtendedEuclid:
         self.x = None
         self.y = None
         self.d = None
-        self.n_x = None
-        self.n_y = None
-        self.n_d = None
 
     def extended_euclid(self, a: int, b: int, d=None, x=None, y=None):
         if b == 0:
             self.d = a
-            self.n_d = a
             self.x = 1
-            self.n_x = 1
             self.y = 0
-            self.n_y = 0
             return a
         else:
-            self.extended_euclid(b, a % b, self.n_d, self.n_x, self.n_y)
-            self.x = self.n_y
-            self.y = self.n_x - (a / b) * self.n_y
-            self.d = self.n_d
+            self.extended_euclid(b, a % b, self.d, self.x, self.y)
+            old_x = self.x
+            self.x = self.y
+            self.y = old_x - (a / b) * self.y
+
             return self.d
